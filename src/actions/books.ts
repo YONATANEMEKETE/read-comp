2321c25e8ecad2913033c755737894a77833e97d
YONATANEMEKETE <yonatanemekete33@gmail.com>
2026-01-30 08:53:58 +0300

    when opening new books, create the relation with reading status

--- src/actions/books.ts
+++ src/actions/books.ts
@@ -61,7 +61,7 @@ export async function getBookWithProgressAction(
         data: {
           userId,
           bookId,
-          status: 'NEW',
+          status: 'READING',
           progressPage: 1,
         },
       });
